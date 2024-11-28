cae58dfc5629800700a9f9a0d3206e79cc19578f
Toru45 <faiqradiansyah@gmail.com>
2024-11-28 23:32:21 +0700

    rename refresh token API

--- routes/index.js
+++ routes/index.js
@@ -8,7 +8,7 @@ const router = express.Router();
 router.get('/api/v1/users',verifyToken,users)
 router.post('/api/v1/register',register)
 router.post('/api/v1/login',login)
-router.get('/api/v1/refreshtoken',refreshToken)
+router.get('/api/v1/token',refreshToken)
 router.delete('/api/v1/logout',logout)
 router.post('/api/v1/userdata', userdata)
 
