9767cf3d396b3a35482c3f0db4d8818aa59e2a69
Toru45 <faiqradiansyah@gmail.com>
2024-12-12 22:40:00 +0700

    docs: API documentation with OPEN API

--- routes/index.js
+++ routes/index.js
@@ -24,12 +24,13 @@ router.post('/api/v1/login', login)
 router.get('/api/v1/token', refreshToken)
 router.delete('/api/v1/logout',logout)
 router.post('/api/v1/userdata', saveUserData)
+router.get('/api/v1/userdata', verifyToken, getUserData);
+router.post('/api/v1/history', saveUserHistory)
 router.get('/api/v1/history', getUserHistory)
 router.patch('/api/v1/changePassword', verifyToken, changePassword)
-router.post('/api/v1/history', saveUserHistory)
 router.post('/api/v1/chronotype', verifyToken, chronotype)
 router.post('/api/v1/sleeprecomendation', verifyToken, sleeprecomenadation)
 router.get('/api/v1/sleeprecommendation', verifyToken, getSleepRecommendation);
-router.get('/api/v1/userdata', verifyToken, getUserData);
+
 export default router
 
